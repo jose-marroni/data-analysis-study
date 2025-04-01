b6d9d054c5683f60b0826a324c1aeac23c73d52a
joseMarroni98 <marroni55@outlook.com>
2025-04-01 11:58:11 -0300

    Create tarefa.sh
    create fale tarefa.sh

---
+++ tarefa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Informações de execução
+
+echo "Iniociando a calculadora"
+
+echo "Informando o diretorio atual: $PWD "
+
+echo " Calculadora ativa informe os numeros e selecione a operação"
+
+python3 /home/jose/modulo1/linux/atividade/calculadora.py
